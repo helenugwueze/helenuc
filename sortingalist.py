6fb79d33dd379c572e7051ac490d9c6a279fc580
helenugwueze <ugwuezehelenchinaza@gmail.com>
2026-09-05 02:37:24 +0100

    sortingalist.py

---
+++ sortingalist.py
@@ -0,0 +1,44 @@
+items = ['ball','dog','cup','apple']
+items.sort(reverse=True)
+print("Here is the original list:")
+print(items)
+print("\nHere is the sorted list:")
+print(sorted(items))
+print("\nHere is the original list again:")
+print(items)
+
+print(items)
+items.reverse()
+print(items)
+
+print(len(items))
+
+
+
+
+
+
+
+countrys = ['dubei','unitedstate','ghana','england']
+print(countrys)
+countrys.sort()
+print(countrys)
+print("here is the original list")
+print(countrys)
+countrys.sort(reverse=True)
+print(countrys)
+print("\nHere is the original list again:")
+print(countrys)
+countrys.reverse()
+print(countrys)
+countrys.reverse()
+print(countrys)
+print("\nHere is the sorted list:")
+print(sorted(countrys))
+countrys.sort()
+print(countrys)
+print("Here is the original list")
+print(countrys)
+countrys.sort()
+print(countrys)
+print("here is the original list:")
